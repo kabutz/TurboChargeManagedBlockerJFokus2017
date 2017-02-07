6e1117b66b7b0b661db1c4cc2e5e8c0558efc6f0
kabutz <heinz@javaspecialists.eu>
2017-02-07 13:10:12 +0100

    demo 1

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -4,14 +4,24 @@
 package eu.javaspecialists.performance.managedblocker;
 
 import java.math.*;
-import java.util.*;
-import java.util.concurrent.*;
 
 public class Fibonacci {
+    /*
+    test100_000_000() time = 45935
+     */
     public BigInteger f(int n) {
         if (n == 0) return BigInteger.ZERO;
         if (n == 1) return BigInteger.ONE;
 
-        return f(n-1).add(f(n-2));
+        int half = (n + 1) / 2;
+
+        BigInteger f0 = f(half - 1);
+        BigInteger f1 = f(half);
+
+        if (n % 2 == 1) {
+            return f0.multiply(f0).add(f1.multiply(f1));
+        } else {
+            return f0.shiftLeft(1).add(f1).multiply(f1);
+        }
     }
 }
